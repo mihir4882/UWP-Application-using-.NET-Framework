fa2b66d23ce8af6ac0a6952d633daf8ad8b10cf5
Mihir Patel <pate1317@sheridancollege.ca>
2020-08-26 12:28:58 -0400

    Git init

---
+++ FinalUWPMihirPatel/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace FinalUWPMihirPatel
+{
+  
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Radiobtn1.IsChecked == true)
+            {
+                double Fahrenheit = Math.Round((double.Parse(tb.Text) * (9.0 / 5.0)) + 32,2);
+                TextOutput.Text = Fahrenheit + string.Format("\u00B0 F");
+            }
+
+            else if (Radiobtn2.IsChecked == true)
+            {
+                double Celsius = Math.Round((double.Parse(tb.Text) - 32) * (5.0 / 9.0),2);
+                TextOutput.Text = Celsius + string.Format("\u00B0 C");
+            }
+        }
+    }
+}
